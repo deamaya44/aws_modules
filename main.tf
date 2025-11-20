5f122570e53474e013c153c458b564f37bf5074b
David Amaya <david.amaya@axity.com>
2025-11-20 08:33:01 -0500

    Commit main.tf -- it's a sample main

--- main.tf
+++ main.tf
@@ -1,24 +1,27 @@
-module "vpc" {
-source   = "git::ssh://git@github.com/deamaya44/aws_modules.git//modules/vpc?ref=main"
-#   for_each = local.vpc --- Uncomment this, when you set the locals ---
-  # VPC Configuration
-  vpc_name             = each.key
-  vpc_cidr             = each.value.vpc_cidr
-  enable_dns_hostnames = each.value.enable_dns_hostnames
-  enable_dns_support   = each.value.enable_dns_support
+########### Sample main.tf file for using AWS modules ###########
 
-  # Internet Gateway
-  create_igw = each.value.create_igw
+# module "vpc" {
+# source   = "git::ssh://git@github.com/deamaya44/aws_modules.git//modules/vpc?ref=main"
+# #   for_each = local.vpc --- Uncomment this, when you set the locals ---
+#   # VPC Configuration
+#   vpc_name             = each.key
+#   vpc_cidr             = each.value.vpc_cidr
+#   enable_dns_hostnames = each.value.enable_dns_hostnames
+#   enable_dns_support   = each.value.enable_dns_support
 
-  # Subnets
-  public_subnet_cidrs     = each.value.public_subnet_cidrs
-  private_subnet_cidrs    = each.value.private_subnet_cidrs
-  availability_zones      = each.value.availability_zones
-  map_public_ip_on_launch = each.value.map_public_ip_on_launch
+#   # Internet Gateway
+#   create_igw = each.value.create_igw
 
-  # NAT Gateway
-  create_nat_gateway = each.value.create_nat_gateway
+#   # Subnets
+#   public_subnet_cidrs     = each.value.public_subnet_cidrs
+#   private_subnet_cidrs    = each.value.private_subnet_cidrs
+#   availability_zones      = each.value.availability_zones
+#   map_public_ip_on_launch = each.value.map_public_ip_on_launch
 
-  # Common Tags
-  common_tags = each.value.tags
-}
+#   # NAT Gateway
+#   create_nat_gateway = each.value.create_nat_gateway
+#   create_s3_endpoint = each.value.create_s3_endpoint
+
+#   # Common Tags
+#   common_tags = each.value.tags
+# }
