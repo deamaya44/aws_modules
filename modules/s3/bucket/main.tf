feba4d049ef05ecd8d6735639e4f5d7172f26c46
David Amaya <amayadavid44@gmail.com>
2025-11-28 22:33:03 -0500

    Fix: s3 basic modules

---
+++ modules/s3/bucket/main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.name
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count  = var.policy != null ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  policy = var.policy
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = var.versioning ? "Enabled" : "Suspended"
+  }
+}
+
+# Public Access Block (optional)
+resource "aws_s3_bucket_public_access_block" "this" {
+  count  = var.block_public_access ? 1 : 0
+  bucket = aws_s3_bucket.this.id
+  
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
---
+++ modules/s3/bucket/variables.tf
@@ -0,0 +1,28 @@
+variable "name" {
+    description = "Name of the S3 bucket"
+    type        = string
+}
+
+variable "tags" {
+    description = "Tags to apply to the S3 bucket"
+    type        = map(string)
+    default     = {}
+}
+
+variable "policy" {
+    description = "The bucket policy to apply to the S3 bucket"
+    type        = string
+    default     = null
+}
+
+variable "versioning" {
+    description = "Enable versioning for the S3 bucket"
+    type        = bool
+    default     = false
+}
+
+variable "block_public_access" {
+    description = "Enable public access block for the S3 bucket"
+    type        = bool
+    default     = true
+}
