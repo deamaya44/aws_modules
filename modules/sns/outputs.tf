76ce1540867d2883d75a7d7707eb7d9df6ec64ba
David Amaya <david.amaya@linktic.com>
2026-02-21 05:46:10 -0500

    refactor: remove composite modules (api-backend, sns)

--- modules/sns/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "topic_arn" {
-  description = "SNS topic ARN"
-  value       = aws_sns_topic.this.arn
-}
-
-output "topic_name" {
-  description = "SNS topic name"
-  value       = aws_sns_topic.this.name
-}
